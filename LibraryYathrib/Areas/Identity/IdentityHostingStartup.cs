0d65b0112a8c36bfa7b492fec267594127cfdc00
FezaanSKY <60107735+FezaanSKY@users.noreply.github.com>
2020-12-20 13:40:03 +0000

    First commit

---
+++ LibraryYathrib/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using LibraryYathrib.Areas.Identity.Data;
+using LibraryYathrib.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(LibraryYathrib.Areas.Identity.IdentityHostingStartup))]
+namespace LibraryYathrib.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AuthDbContext>();
+            });
+        }
+    }
+}
+
